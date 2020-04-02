9f4ed8ff3ea426b69750724b3b94967661cd388c
Henrique Neto <dmf.n123@gmail.com>
2020-04-02 01:41:14 -0300

    Segunda Tela criada

---
+++ app/src/main/java/com/example/fiqueemcasa/AddDataInFB.java
@@ -0,0 +1,14 @@
+package com.example.fiqueemcasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDataInFB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro);
+    }
+}
--- app/src/main/java/com/example/fiqueemcasa/MainActivity.java
+++ app/src/main/java/com/example/fiqueemcasa/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.fiqueemcasa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCadastrar = (Button) findViewById(R.id.buttonCadastrar);
+    }
+
+    public void cadastrar(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, AddDataInFB.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 }
